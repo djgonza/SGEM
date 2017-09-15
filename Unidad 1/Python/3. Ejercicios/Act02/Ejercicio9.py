2b4bef10b8d5f8b01605e958c2687f70056b51ea
djgonza <davidjimenezgonza@gmail.com>
2017-09-15 12:43:15 +0200

    Update

--- Unidad 1/Python/3. Ejercicios/Act02/Ejercicio9.py
+++ Unidad 1/Python/3. Ejercicios/Act02/Ejercicio9.py
@@ -1,6 +1,7 @@
 opt = input("Circulo (c) Triangulo (t)")
 if opt == "c":
     rad = float(input("Inserte un radio: "))
+
 elif:
     base = float(input("Inserte la base"))
     altura = float(input("Inserte la altura"))
