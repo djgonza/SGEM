e3cbf40dbc2615122e292e84190ffdb161603058
djgonza <davidjimenezgonza@gmail.com>
2017-09-29 10:36:51 +0200

    UPDATE SGEM
    Ejercicios

--- Unidad 1/Python/3. Ejercicios/Act07/Ejercicio4/App.py
+++ Unidad 1/Python/3. Ejercicios/Act07/Ejercicio4/App.py
@@ -1,10 +1,15 @@
 from Act07.Ejercicio4.Numero import Numero
-#Este ejercicio determina si el número que se le pasa es un número primo.
-num = int (input("Introduzca un número: "))
+#Este ejercicio determina si el número que se le pasa es un número primo y nos muestrael nº de primos introducidos y el total
+while True:
+    num = int(input("Introduzca un número(0: Salir): "))
 
-numero = Numero(num)
+    if num == 0:
+        break;
 
-if numero.esPrimo():
-    print("El numero {} es un número primo.".format(num))
-else:
-    print("El numero {} NO es un número primo.".format(num))
+    numero = Numero(num);
+    if numero.esPrimo():
+        print("El numero {} es un número primo.".format(num))
+    else:
+        print("El numero {} NO es un número primo.".format(num))
+
+print("Total numeros introducidos {}, de los cuales {} son primos".format(Numero.totalNumeros, Numero.cuantosPrimos))
--- Unidad 1/Python/3. Ejercicios/Act07/Ejercicio4/Numero.py
+++ Unidad 1/Python/3. Ejercicios/Act07/Ejercicio4/Numero.py
@@ -1,21 +1,26 @@
 class Numero (object):
 
+    cuantosPrimos = 0;
+    totalNumeros = 0;
+
     def __init__(self, num):
-        self.num = num
+        self.num = num;
+        Numero.totalNumeros += 1;
 
     def esPrimo(self):
-        cont = 0
-        i = 2
+        cont = 0;
+        i = 2;
         if self.num == 1:
-            return bool(0)
+            return bool(0);
         else:
             while True:
                 if i> self.num/2:
-                    break
+                    break;
 
                 if self.num % i == 0:
-                    cont += 1
+                    cont += 1;
                 if cont != 0:
-                    return bool(0)
+                    return bool(0);
                 i += 1
-        return bool(1)
+        Numero.cuantosPrimos += 1;
+        return bool(1);
